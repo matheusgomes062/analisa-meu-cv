93bd65ef790bfe0c828da6347f2246c284eb15d9
Matheus Gomes <matheusgomes062@gmail.com>
2024-04-23 17:55:59 -0300

    chore: first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  transpilePackages: ['lucide-react']
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,27 @@
+import { User } from "@prisma/client"
+import type { Icon } from "lucide-react"
+
+import { Icons } from "@/components/icons"
+
+export type NavItem = {
+  title: string
+  href: string
+  disabled?: boolean
+}
+
+export type MainNavItem = NavItem
+
+export type SiteConfig = {
+  name: string
+  description: string
+  url: string
+  ogImage: string
+  links: {
+    linkedin: string
+    github: string
+  }
+}
+
+export type MarketingConfig = {
+  mainNav: MainNavItem[]
+}
